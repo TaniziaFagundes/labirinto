9c50333351f1424cfe95794e6303112a72bc8380
Tanizia Fagundes <tdlf@icomp.ufam.edu.br>
2021-04-01 18:18:25 -0400

    correct move now

--- script.js
+++ script.js
@@ -23,11 +23,12 @@ let left = 0;
 let cont = 0
 let posicao_Linha = 9;
 let posicao_Coluna = 0;
-let aux_coluna = 0
-let aux_linha = 0
+let aux_coluna = 0;
+let aux_linha = 0;
 
 
 var element_pai = document.getElementById("labirinto");
+
 function criarYoda(){
     var inicio = document.getElementById("inicio");
     var newdiv = document.createElement("div");
@@ -43,6 +44,7 @@ function criarYoda(){
     newdiv.position = "relative"
     inicio.appendChild(newdiv);
 }
+
 function criarLab(){
     for (i=0;i<map.length;i++){
         let aux = map[i]
@@ -97,6 +99,7 @@ criarYoda()
 
 function movimentoPermitido(linha,coluna){
     let permisao;
+    console.log(map[linha][coluna])
        if( map[linha][coluna] !="W" && map[linha][coluna] !=null){
            permisao =  true;
        }else{
@@ -108,47 +111,47 @@ function movimentoPermitido(linha,coluna){
 document.addEventListener("keydown", (event)=>{
     let keyName = event.key;
     
-        
 
         if(keyName == "ArrowLeft"){
 
-            aux_coluna+= posicao_Coluna - 1;
-            if(movimentoPermitido(posicao_Linha,aux_coluna) == true){
+            posicao_Coluna -= 1;
+            if(movimentoPermitido(posicao_Linha,posicao_Coluna) == true){
                 document.getElementById("yoda").style.marginLeft = (left-=30) + "px";
             }else{
-                aux_coluna+= posicao_Coluna + 1;
+                posicao_Coluna += 1;
             }
               
         }else if(keyName == "ArrowRight"){
 
-            aux_coluna += posicao_Coluna + 1;
-            if(movimentoPermitido(posicao_Linha,aux_coluna) == true){
+            posicao_Coluna += 1;
+            if(movimentoPermitido(posicao_Linha,posicao_Coluna) == true){
                 document.getElementById("yoda").style.position = "relative";
                 document.getElementById("yoda").style.marginLeft = (left+=30) + "px";
             }else{
-                aux_coluna += posicao_Coluna - 1;
+                posicao_Coluna -= 1;
             }
               
         }
         //essa parte esta bugado
         if(keyName == "ArrowDown"){
 
-            aux_linha += posicao_Linha + 1;
-            console.log(movimentoPermitido(aux_linha,posicao_Coluna))
-            if(movimentoPermitido(aux_linha,posicao_Coluna) == true){
-                document.getElementById("yoda").style.marginTop = (topdiv+=100) + "px";
+            posicao_Linha += 1;
+            console.log(movimentoPermitido(posicao_Linha,posicao_Coluna))
+            if(movimentoPermitido(posicao_Linha,posicao_Coluna) == true){
+                console.log("areadown");
+                document.getElementById("yoda").style.marginTop = (topdiv+=30) + "px";
             }else{
-                aux_linha += posicao_Linha - 1;
+                posicao_Linha -= 1;
             }
         
             
         }else if(keyName == "ArrowUp"){
             
-            aux_linha += posicao_Linha + 1;
-            if(movimentoPermitido(aux_linha,posicao_Coluna) == true){
+            posicao_Linha -= 1;
+            if(movimentoPermitido(posicao_Linha,posicao_Coluna) == true){
                 document.getElementById("yoda").style.marginTop = (topdiv-=30) + "px";
             }else{
-                aux_linha += posicao_Linha - 1;
+                posicao_Linha += 1;
             }
         }   
 
